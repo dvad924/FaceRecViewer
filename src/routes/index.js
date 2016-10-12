80b97b706db51d889e67b8142bc921f342d4152e
alex <am3081@gmail.com>
2016-10-12 16:47:07 -0400

    login screen

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@
 import CoreLayout from '../layouts/CoreLayout/CoreLayout'
 import Home from './Home'
 import CounterRoute from './Counter'
-
+import Login from './Login/index'
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
 
@@ -11,7 +11,8 @@ export const createRoutes = (store) => ({
   component   : CoreLayout,
   indexRoute  : Home,
   childRoutes : [
-    CounterRoute(store)
+    CounterRoute(store),
+    Login(store)
   ]
 })
 
